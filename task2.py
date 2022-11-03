3487dace7fd15cc062aa2c4f29f72bf0e46f1b71
daniil_dn <dosindaniilm@gmail.com>
2022-11-03 23:48:38 +0300

    unittests

--- task2.py
+++ task2.py
@@ -6,8 +6,10 @@
 from pprint import pprint
 import random
 import math
+import unittest
+import timeit
 
-TIMESTAMPS_COUNT = 50
+TIMESTAMPS_COUNT = 50000
 
 PROBABILITY_SCORE_CHANGED = 0.0001
 
@@ -57,6 +59,7 @@ pprint(game_stamps)
 
 def get_score(game_stamps, offset):
     '''
+        Binary search in stamps for offset
         Takes list of game's stamps and time offset for which returns the scores for the home and away teams.
         Please pay attention to that for some offsets the game_stamps list may not contain scores.
     '''
@@ -78,4 +81,42 @@ def get_score(game_stamps, offset):
         'away']  # if searching offset is not in stamps list -> return the nearest offset data
 
 
-print(get_score(game_stamps, 1))
+def get_score_lin(game_stamps, offset):
+    for i in game_stamps:
+        if i['offset'] == offset:
+            return i['score']['home'], i['score']['away']
+
+
+# print(timeit.timeit(stmt="get_score_lin(game_stamps, 30000)", globals=globals()))
+# print(timeit.timeit(stmt="get_score(game_stamps, 30000)", globals=globals()))
+print(get_score_lin(game_stamps, 30000))
+print(get_score(game_stamps, 30000))
+
+
+class TestGetScore(unittest.TestCase):
+    def setUp(self) -> None:
+        self.game_stamps = generate_game()
+        self.get_score = get_score
+
+    def test_offset_right(self):
+        offset = 50000
+        self.assertIsNotNone(self.get_score(self.game_stamps, offset))
+
+    def test_offset_left(self):
+        offset = 1
+        self.assertIsNotNone(self.get_score(self.game_stamps, offset))
+
+    def test_offset_mid(self):
+        offset = len(self.game_stamps) // 2
+        self.assertIsNotNone(self.get_score(self.game_stamps, offset))
+
+    def test_offset_first(self):
+        offset = 0
+        self.assertEqual(self.get_score(self.game_stamps, offset), get_score_lin(self.game_stamps, offset))
+
+    def test_all(self):
+        i = 0
+        while i < len(self.game_stamps):
+            offset = self.game_stamps[i]['offset']
+            self.assertEqual(self.get_score(self.game_stamps, offset), get_score_lin(self.game_stamps, offset))
+            i += len(self.game_stamps) // 100
